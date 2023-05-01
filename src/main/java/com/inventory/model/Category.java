f82cda2926a74490ac876ce82f9c846b06e83f3a
Alejandra <10alejandraflorez@gmail.com>
2023-05-01 11:21:17 -0500

    Agregar modelo Category

---
+++ src/main/java/com/inventory/model/Category.java
@@ -0,0 +1,28 @@
+package com.inventory.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String picture;
+
+}
