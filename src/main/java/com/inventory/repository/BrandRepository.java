eaa0444d28e8d8a0498e398671d15f3968b258db
Alejandra <10alejandraflorez@gmail.com>
2023-05-06 20:21:37 -0500

    Se agrega anotacion @Repository

--- src/main/java/com/inventory/repository/BrandRepository.java
+++ src/main/java/com/inventory/repository/BrandRepository.java
@@ -2,6 +2,8 @@ package com.inventory.repository;
 
 import com.inventory.model.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 }
--- src/main/java/com/inventory/repository/OrderDetailRepository.java
+++ src/main/java/com/inventory/repository/OrderDetailRepository.java
@@ -2,6 +2,8 @@ package com.inventory.repository;
 
 import com.inventory.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
 }
